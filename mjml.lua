8ac3fd84829112108d4652e6dfa122b38d2e80b3
William Gregorio <mail@williamgregorio.com>
2024-09-03 00:38:47 -0400

    initial test node

--- mjml.lua
+++ mjml.lua
@@ -4,4 +4,12 @@ local t = text_node
 local i = insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 
-
+ls.add_snippets("mjml", {
+  s("mjtest", fmt(
+    [[ 
+    <mj-test>
+      {}
+    </mj-test>
+    ]], {i{1}}
+  ))
+})
